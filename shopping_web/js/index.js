fc010e06b063ac2d2d23b8738ecd144ec22b2ed1
polegithub <tschengkong@163.com>
2016-10-26 23:34:33 +0800

    [ADD] add shopping trolley view

--- shopping_web/js/index.js
+++ shopping_web/js/index.js
@@ -1,11 +1,12 @@
 
 function showTotalMenuAction(){
-    if(showdiv_display = document.getElementsByClassName('shopClass_show').style.display=='none'){
-    //如果show是隐藏的 
-    document.getElementsByClassName('shopClass_show').style.display='block';
-    //show的display属性设置为block（显示）
-    }else{
-    //如果show是显示的，show的display属性设置为none（隐藏）
-    document.getElementsByClassName('shopClass_show').style.display='none';
-    }
+
+    // if(showdiv_display = document.getElementsByClassName('shopClass_show').style.display=='none'){
+    //   document.getElementsByClassName('shopClass_show').style.display='block';
+    // }else{
+    //   document.getElementsByClassName('shopClass_show').style.display='none';
+    // }
+    if (showdiv_display = document.getElementsById('testId')) {
+      alert("testId");
+    };
  }
